f42ec0a9e7e85a1937b43adbfb9fd8af00c97685
guohongqi9010 <guohongqi9010@zto.com>
2020-07-23 15:04:43 +0800

    Initial Commit

---
+++ MVPDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  MVPDemo
+//
+//  Created by guohongqi on 2020/7/23.
+//  Copyright © 2020 guohongqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
